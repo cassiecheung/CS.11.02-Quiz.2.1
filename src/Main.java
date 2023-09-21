1b91370ad7c078dac9d381ce3d21923d8fa30928
apple <ckyc40@student.cis.edu.hk>
2023-09-21 08:49:28 +0800

    initial commit done! 🐳

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+/* I have neither given nor received any unauthorised aid on this piece of work.*/
 public class Main {
-
     public static void main(String[] args) {
-
+        System.out.println("Please enter one number you would like to be added.");
+        Scanner scanner = new Scanner(System.in);
+        int numOne = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter another number you would like to be added to the previous number.");
+        int numTwo = Integer.parseInt(scanner.nextLine());
+        int resultOne = add(numOne, numTwo);
+        System.out.println(resultOne);
+        System.out.println("Please enter your name!");
+        String name = scanner.nextLine();
+        String resultTwo = greeting(name);
+        System.out.println(resultTwo);
+        System.out.println("Please enter one number you would like to be added.");
+        int numberOne = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter another number you would like to be added to the previous number.");
+        int numberTwo = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter another number you would like to be added to the previous numbers.");
+        int numberThree = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter the last number you would like to be added to the previous numbers.");
+        int numberFour = Integer.parseInt(scanner.nextLine());
+        int resultThree = add(numberOne, numberTwo, numberThree, numberFour);
+        System.out.println(resultThree);
+        System.out.println("Please enter anything you would like to be printed to the console.");
+        String toBePrinted = scanner.nextLine();
+        String resultFour = printme(toBePrinted);
+        System.out.println(resultFour);
     }
-
+    public static int add(int numOne,int numTwo) {
+        return numOne + numTwo;
+    }
+    public static String greeting(String name) {
+        return "Bonjour, " + name + "!";
+    }
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        int x = add(0,numOne);
+        int y = add(numTwo, numThree);
+        int z = add(0, numFour);
+        return x + y + z;
+    }
+    public static String printme(String toBePrinted) {
+        return toBePrinted;
+    }
 }
